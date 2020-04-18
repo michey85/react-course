4a19e6b9a42c36754d306d8e37507f52d718c021
Mikhail Nepomnyashchiy <michey85@gmail.com>
2020-04-18 21:35:09 +0300

    demo github api users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import Clicker from './Clicker';
-import ClickerFn from './ClickerFn';
+import Search from './components/Search';
+import UserList from './components/UserList';
 
 const App = () => {
+    const [users, setUsers] = useState([]);
+    const [isOld, setIsOld] = useState(false);
+    const [loading, setLoading] = useState(false);
+
     return (
-        <div className='myApp'>
-            <Clicker />
-        </div>
+        <>
+            <Search
+                placeholder='Введите запрос'
+                cb={setUsers}
+                old={setIsOld}
+                setLoading={setLoading}
+            />
+            <UserList users={users} old={isOld} loading={loading} />
+        </>
     );
 };
 
